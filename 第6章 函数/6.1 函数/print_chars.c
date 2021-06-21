a7b2b6a54e1d534b5101ec1bda584d3d74a5e33e
xdai02 <2513276112@qq.com>
2021-06-21 11:05:29 +0800

    standard filename

--- 第6章 函数/6.1 函数/print_chars.c
+++ 第6章 函数/6.1 函数/print_chars.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void printStars(int row, int col, char c) {
+void printChars(int row, int col, char c) {
     for(int i = 0; i < row; i++) {
         for(int j = 0; j < col; j++) {
             printf("%c", c);
@@ -10,6 +10,6 @@ void printStars(int row, int col, char c) {
 }
 
 int main() {
-    printStars(5, 10, '?');
+    printChars(5, 10, '?');
     return 0;
 }
