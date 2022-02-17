1506d091e555c075517e8d697639adbe99965b6a
xdai02 <2513276112@qq.com>
2022-02-17 19:34:17 +0800

    reformat chapter 6

--- code/第6章 函数/6.3 递归/ackermann.c
+++ code/第6章 函数/6.3 递归/ackermann.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/**
- * Ackermann's Function 
- *           ①        n + 1             m = 0
- * A(m, n) = ②      A(m-1, 1)           m > 0, n = 0
- *           ③  A(m-1, A(m, n-1))       m > 0, n > 0
- */
 int A(int m, int n) {
     if(m == 0) {
         return n + 1;
