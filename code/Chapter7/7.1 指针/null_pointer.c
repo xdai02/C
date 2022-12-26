8c1abae4f0f538de107a86e34a58ec55807ad6ba
xdai02 <2513276112@qq.com>
2022-12-27 00:46:19 +0800

    chapter 7-1 updated

--- code/Chapter7/7.1 指针/null_pointer.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int *p = NULL;
-    printf("%p\n", p);
-    return 0;
-}
--- code/Chapter7/7.1 指针/wild_pointer.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int *p;
-    printf("%d\n", *p);
-    return 0;
-}
