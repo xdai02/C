15d1f3672b575db17eb3ad22a429fb4a878ff0a4
xdai02 <2513276112@qq.com>
2023-01-09 16:55:03 +0800

    fix typo

--- code/Chapter1/1-3/rectangle.c
+++ code/Chapter1/1-3/rectangle.c
@@ -4,9 +4,7 @@ int main()
 {
     int length = 10;
     int width = 5;
-    double area;
-
-    area = length * width;
+    double area = length * width;
     printf("Area = %d * %d = %.2f\n", length, width, area);
     return 0;
 }
